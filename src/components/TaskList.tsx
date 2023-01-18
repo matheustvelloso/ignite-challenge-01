f40f755f89bcb4c59519accdd1e98cb054c8633e
Matheus Velloso <matheustvelloso@gmail.com>
2023-01-18 10:24:33 -0300

    Challenge 01 is completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,17 +15,27 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle.length < 1) return;
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => task.id === id ? { ...task, isComplete: !task.isComplete } : task)
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const newTasks = [...tasks];
+    newTasks.splice(id, 1);
+    setTasks(newTasks)
   }
-
+  console.log(tasks)
   return (
     <section className="task-list container">
       <header>
